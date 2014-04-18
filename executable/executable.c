25be8af484a8bd8e92c3c293186a37366b89813e
Yixiang Lu <yxlu@google.com>
2014-04-18 16:23:39 -0700

    added setup

---
+++ executable/executable.c
@@ -0,0 +1,16 @@
+//usr/bin/cc -std=c99 -o /tmp/`basename "$0"` $0;/tmp/`basename "$0"` $*;exit
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    if(argc > 1) {
+        printf("arguments:\n");
+        for(int i = 1; i < argc; ++i) {
+            printf("    %s\n", argv[i]);
+        }
+        return 0;
+    } else {
+        printf("missing arguments\n");
+        return 1;
+    }
+}
---
+++ executable/executable.java
@@ -0,0 +1,16 @@
+//usr/bin/javac -d /tmp $0;java -cp /tmp `basename "$0" .java` $*;exit
+
+import static java.lang.System.out;
+
+public class executable {
+    public static void main(String[] args) {
+        if(args.length > 0) {
+            out.println("arguments:");
+            for(String arg : args) {
+                out.println("    " + arg);
+            }
+        } else {
+            out.println("missing arguments");
+        }
+    }
+}
