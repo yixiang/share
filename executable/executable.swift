4dfad331c5d81b451b5a3a0a1c9b48ec8d4f83a2
Sean <luyixiang@gmail.com>
2014-06-11 17:45:04 -0700

    No message

--- executable/executable.swift
+++ executable/executable.swift
@@ -1,5 +1,10 @@
-//usr/bin/env xcrun swift -o /tmp/`basename "$0"` $0;/tmp/`basename "$0"` $*;exit
+//usr/bin/env xcrun swift -o/tmp/`basename "$0"` $0;/tmp/`basename "$0"` $*;exit
 
-for i in 0..C_ARGC {
-    println(C_ARGV[Int(i)])
+if C_ARGC == 1 {
+    println("missing arguments")
+} else {
+    println("arguments:")
+    for i in 1..C_ARGC {
+        println("    \(C_ARGV[Int(i)])")
+    }
 }
